34ca11bd8e8e4e231cba126622b2ec8b0839579f
spidercrew <baimek88@gmail.com>
2018-05-06 17:43:14 +0700

    Update pagebutton.js

--- pagebutton.js
+++ pagebutton.js
@@ -1,3 +1,27 @@
 //<![CDATA[
 eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(1($){h 8=$(\'a.v-b\');h 6=$(\'a.t-b\');$.f(8.7(\'5\'),1(c){8.4(\'<3>u</3> <2>\'+$(c).g(\'.0 i.0-j\').9()+\'</2>\')},"4");$.f(6.7(\'5\'),1(l){6.4(\'<3>s</3> <2>\'+$(l).g(\'.0 i.0-j\').9()+\'</2>\')},"4")})(r);$(o).p(1(){q($("#n,.n").7("5")!="m://k.e.d/"){w.x.5="m://k.e.d"}});',34,34,'post|function|span|strong|html|href|olderLink|attr|newerLink|text||link|data|site|zaqi|get|find|var|h1|title|www|data2|http|creditoop|document|ready|if|jQuery|Previous|older|Next|newer|window|location'.split('|'),0,{}))
+//
+$(function(){
+ 
+    $(document).on( 'scroll', function(){
+ 
+        if ($(window).scrollTop() > 100) {
+            $('.smoothscroll-top').addClass('show');
+        } else {
+            $('.smoothscroll-top').removeClass('show');
+        }
+    });
+ 
+    $('.smoothscroll-top').on('click', scrollToTop);
+});
+ 
+function scrollToTop() {
+    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $('body');
+    offset = element.offset();
+    offsetTop = offset.top;
+    $('html, body').animate({scrollTop: offsetTop}, 600, 'linear');
+}
 //]]>
+
+
