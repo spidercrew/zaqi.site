7222d0c06f4668e87f475ddc816ad88485d1b8c0
spidercrew <baimek88@gmail.com>
2018-05-08 23:25:01 +0700

    Update comment.js

--- assets/comment.js
+++ assets/comment.js
@@ -12,4 +12,18 @@ function blockLinks(parentID, children) {
  }
 }
 blockLinks('comments', 'p');
+//
+$('i[rel="pre"]').replaceWith(function() {
+    return $('<pre><code>' + $(this).html() + '</code></pre>');
+});
+var pres = document.querySelectorAll('pre,kbd,blockquote');
+for (var i = 0; i < pres.length; i++) {
+  pres[i].addEventListener("dblclick", function () {
+    var selection = getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(this);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }, false);
+}
 //]]>
