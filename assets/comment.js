d9587a5cec7845ed12169a6349e014c3b55480bd
spidercrew <baimek88@gmail.com>
2018-05-09 03:16:41 +0700

    Update comment.js

--- assets/comment.js
+++ assets/comment.js
@@ -26,26 +26,4 @@ for (var i = 0; i < pres.length; i++) {
     selection.addRange(range);
   }, false);
 }
-//
-$(function(){
- 
-    $(document).on( 'scroll', function(){
- 
-        if ($(window).scrollTop() > 100) {
-            $('.smoothscroll-top').addClass('show');
-        } else {
-            $('.smoothscroll-top').removeClass('show');
-        }
-    });
- 
-    $('.smoothscroll-top').on('click', scrollToTop);
-});
- 
-function scrollToTop() {
-    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
-    element = $('body');
-    offset = element.offset();
-    offsetTop = offset.top;
-    $('html, body').animate({scrollTop: offsetTop}, 600, 'linear');
-}
 //]]>
