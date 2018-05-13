b44ee3c7cf2a18ce7b9b48e7fe737f3a7b62f772
spidercrew <baimek88@gmail.com>
2018-05-13 15:27:24 +0700

    Update top.js

--- top.js
+++ top.js
@@ -31,4 +31,6 @@ function blockLinks(parentID, children) {
  }
 }
 blockLinks('comments', 'p');
+//
+function showComm(e){document.getElementById(e)&&("none"!=document.getElementById(e+"-show").style.display?(document.getElementById(e+"-show").style.display="none",document.getElementById(e).style.display="block"):(document.getElementById(e+"-show").style.display="block",document.getElementById(e).style.display="none"))};
 //]]>
